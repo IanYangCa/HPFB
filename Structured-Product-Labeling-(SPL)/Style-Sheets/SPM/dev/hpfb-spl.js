369f36cb953ca469b0439647897cc70a44af44e5
IanYangCa <yin.yang@canada.ca>
2017-12-14 09:32:38 -0500

    Show watermark cross all area.

---
+++ Structured-Product-Labeling-(SPL)/Style-Sheets/SPM/dev/hpfb-spl.js
@@ -0,0 +1,14 @@
+function setWatermarkBorder(){
+	var watermarks = document.getElementsByClassName('Watermark');
+	if(! empty(watermarks)){
+		watermarks.forEach(initialWatermark());
+	}
+}
+function initialWatermark(item, index){
+	var table = item.getElementsByTagName('table');
+	var tableBorder = table.getBoundingClientRect();
+	item.style.left = tableBorder.left;
+	item.style.top = tableBorder.top;
+	item.style.right = tableBorder.right;
+	item.style.bottom = tableBorder.bottom;
+}
