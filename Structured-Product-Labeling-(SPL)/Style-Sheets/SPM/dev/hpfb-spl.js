88bf5d0256215008daadd2996ccf69a355be933d
IanYangCa <yin.yang@canada.ca>
2017-12-21 14:52:31 -0500

    Clean code

--- Structured-Product-Labeling-(SPL)/Style-Sheets/SPM/dev/hpfb-spl.js
+++ Structured-Product-Labeling-(SPL)/Style-Sheets/SPM/dev/hpfb-spl.js
@@ -13,10 +13,9 @@ function initialWatermark(item, index){
 		var angle = Math.atan2($(table).height(),$(table).width())*180/Math.PI;
 		var v_offset = $(table).height()/2 + $(watermarkText).height()/2; //$(table).height() / 2 + Math.cos(angle)*width/2;
 		var h_offset = - (width - $(table).width())/2; //Math.sin(angle)*width/2 + $(table).width()/2;
-		$(watermarkText).css({top : v_offset, left: h_offset});
 		$(watermarkText).css("width", width);
+		$(watermarkText).css({top : v_offset, left: h_offset});
 		$(watermarkText).show();
 		$(watermarkText).css({'transform' :  'rotate(-' + angle + 'deg)'});
-//		$(watermarkText).css("height", $(table).width());
 	}
 }
