75ca1a5986d586b634873e5bb976c5f488f4ce52
IanYangCa <yin.yang@canada.ca>
2018-03-12 10:50:46 -0400

    Change join product names from ", " to ";  "

--- Structured-Product-Labeling-(SPL)/Style-Sheets/SPM/dev.2/hpfb-spl.js
+++ Structured-Product-Labeling-(SPL)/Style-Sheets/SPM/dev.2/hpfb-spl.js
@@ -53,7 +53,7 @@ function twoColumnsDisplay(){
 		headerTitle = "";
 		$("h2 a[href^='#product']").each(function(){
 			if(insertComa){
-				headerTitle += ", "
+				headerTitle += ";  "
 			}
 			insertComa = true;
 			title = $(this).html();
