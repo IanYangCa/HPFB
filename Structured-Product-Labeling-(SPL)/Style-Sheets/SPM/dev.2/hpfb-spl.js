828e89b11be684a4e7940f9be007375114d9e611
IanYangCa <yin.yang@canada.ca>
2018-03-12 13:59:50 -0400

    add more space between product names

--- Structured-Product-Labeling-(SPL)/Style-Sheets/SPM/dev.2/hpfb-spl.js
+++ Structured-Product-Labeling-(SPL)/Style-Sheets/SPM/dev.2/hpfb-spl.js
@@ -53,7 +53,7 @@ function twoColumnsDisplay(){
 		headerTitle = "";
 		$("h2 a[href^='#product']").each(function(){
 			if(insertComa){
-				headerTitle += ";  "
+				headerTitle += ";&nbsp;&nbsp;"
 			}
 			insertComa = true;
 			title = $(this).html();
